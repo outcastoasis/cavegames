9ba7f6a0bf20206e4612ea52301540ac98ebecf3
Jascha Bucher <jascha.bucher55@gmail.com>
2025-10-28 21:42:37 +0100

    Entferne ignorierte Dateien aus Git-Tracking

--- frontend/src/services/api.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
-export async function testAPI() {
-  const res = await fetch(`${BASE_URL}/test`);
-  if (!res.ok) throw new Error("Fehler beim API-Test");
-  return res.json();
-}
